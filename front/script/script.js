6ffb04ee4e95c28249a4f32fb5f7036de5530d1d
Djibril Samassa <djibrilsamassa.pro@gmail.com>
2022-01-20 12:00:24 +0100

    1 et 2 commit

--- front/script/script.js
+++ front/script/script.js
@@ -6,15 +6,18 @@ $( () =>{
             url: 'https://restcountries.com/v3.1/all',
             success: function(res){
                 for(let i = 0; i<=res.length;i++){
-                        if( $("#searchbycountryname").attr("checked")){
-                            $("#btnShowData").css("background-color","yellow");
-                        }
-                        else if( $("#searchbycityname").attr("checked")){
-                            $("#btnShowData").css("background-color","green");
-                        }
+                    $(".countries_name").append(`<li>${res[i].name.common}</li>`)
                 }
             }
         });
 
     })
+
+    if( $("#searchbycountryname").checked){
+        $("#btnShowData").css("background-color","yellow");
+    }
+    
+    else if( $("#searchbycityname").checked){
+        $("#btnShowData").css("background-color","green");
+    }
 })
