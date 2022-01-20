907abc1924adbcf302d9a594e41112e8023d3189
Djibril Samassa <djibrilsamassa.pro@gmail.com>
2022-01-20 10:43:15 +0100

    Day 2 Commit

--- front/script/script.js
+++ front/script/script.js
@@ -6,16 +6,12 @@ $( () =>{
             url: 'https://restcountries.com/v3.1/all',
             success: function(res){
                 for(let i = 0; i<=res.length;i++){
-                        if( $("#cherchCountry").val() == res[i].name.common){
-                            $(".countries_name").append(`<li>${res[i].name.common} <b> Capitale: </b>${res[i].capital}</li>`)
-                            return;
+                        if( $("#searchbycountryname").attr("checked")){
+                            $("#btnShowData").css("background-color","yellow");
                         }
-
-                        else if( $("#cherchCountry").val() == res[i].capital){
-                            $(".countries_name").append(`<li>${res[i].capital} <b> Pays: </b>${res[i].name.common}</li>`)
-                            return;
+                        else if( $("#searchbycityname").attr("checked")){
+                            $("#btnShowData").css("background-color","green");
                         }
-                    
                 }
             }
         });
